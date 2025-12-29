a256427be6fa030d80ca20874dc28c8c98815cd1
Osandi26 <osandipabasara@gmail.com>
2025-12-30 00:25:52 +0530

    Added React Router and page navigation

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+
 function SearchPage() {
     return (
       <div>
         <h2>Property Search</h2>
+
+        <Link to="/property/prop1">
+            Go to sample property
+        </Link>
         <p>Search form will go here</p>
       </div>
     );
