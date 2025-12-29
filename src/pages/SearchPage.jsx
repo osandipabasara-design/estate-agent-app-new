d8e90e2f18a02fa8cbcb6c5429755a3c249516b4
Osandi26 <osandipabasara@gmail.com>
2025-12-30 00:37:23 +0530

    estate-agent-app-new

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import properties from "../data/properties.json";
 
 function SearchPage() {
     return (
       <div>
         <h2>Property Search</h2>
+        <p>Total Properties: {properties.length}</p>
 
         <Link to="/property/prop1">
             Go to sample property
