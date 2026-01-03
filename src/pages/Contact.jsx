e22ef62abbadd0af71e27e7758a4564fa3508405
Osandi26 <osandipabasara@gmail.com>
2026-01-04 00:38:15 +0530

    estate-agent-new-app

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,43 @@
+import "./Contact.css";
+
 const Contact = () => {
     return (
-      <div className="page">
+      <div className="contact-page">
         <h1>Contact Us</h1>
-        <p>Email: osandipabasara@gmail.com</p>
-        <p>Phone: +1 (800) 123-4567</p>
-        <p>Address: 123 Blue Haven, Ocean City</p>
+
+        <p className="contact-intro">
+            Get in touch with us.
+        </p>
+
+        <div className="contact-section">
+            {/*Contact Form */}
+            <div className="contact-form">
+                <form>
+                    <label for="name">Name : </label>
+                    <input type="text" value="name" placeholder="Enter Your Name" />
+
+                    <label for="email">Email : </label>
+                    <input type="text" value="email" placeholder="example@email.com" />
+
+                    <label for="message">Message : </label>
+                    <textarea placeholder="type your message here"></textarea>
+
+                    <button type="submit">Send Message</button>
+
+                </form>
+            </div>
+        </div>
+
+        {/*Contact Info */}
+        <div className="contact-info">
+            <h2>Contact Information</h2>
+
+            <p><strong>Email : </strong>w2153603@westminster.ac.uk</p>
+            <p><strong>Phone : </strong>+44 20 7946 0958</p>
+            <p><strong>Address : </strong>45 King William Street, Lond, EC4R 9AN, UK</p>
+        </div>
+
+
       </div>
     );
   };
